8963e7226bdbbc7cb660d38d603883a45904fafa
Davi Costa <davim.costa@outlook.com>
2023-08-14 16:57:00 -0300

    Primeiro teste.

---
+++ src/componentes/Formulario.tsx
@@ -0,0 +1,13 @@
+
+const Formulario = () => {
+  return (
+    <form>
+        
+        <input type='text' placeholder='Insira os nomes dos participantes' />
+        <button disabled>Adicionar</button>
+        
+    </form>
+  )
+}
+
+export default Formulario
